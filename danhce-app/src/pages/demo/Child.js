111b2b1b81e7fafe9b00f3fea3a9a740fb6fedbb
lhalou <614124284@qq.com>
2020-10-02 22:08:47 +0800

    暴露webpack及配置相关

--- danhce-app/src/pages/demo/Child.js
+++ danhce-app/src/pages/demo/Child.js
@@ -1,5 +1,7 @@
 import React,{Component} from 'react'
 import './index.less'
+import {Button} from 'antd'
+
 class Child extends Component {
   constructor(props){
     super(props)
@@ -9,6 +11,7 @@ class Child extends Component {
       <div className = 'content'>
         <p>这是子组件</p>
         <p>{this.props.count}</p>
+        <Button>按钮</Button>
       </div>
     )
   }
