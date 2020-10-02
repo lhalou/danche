0df1a46c84da78fa2a667f8643ee0b038fcdccbb
lhalou <614124284@qq.com>
2020-10-02 16:30:57 +0800

    less文件不生效

--- danhce-app/src/pages/demo/Child.js
+++ danhce-app/src/pages/demo/Child.js
@@ -1,11 +1,12 @@
 import React,{Component} from 'react'
+import './index.less'
 class Child extends Component {
   constructor(props){
     super(props)
   }
   render(){
     return (
-      <div>
+      <div className = 'content'>
         <p>这是子组件</p>
         <p>{this.props.count}</p>
       </div>
